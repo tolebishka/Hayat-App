73aed3992da54dfa71e16944d9a3e5e6235ec4a6
tolebishka <aitenovtolebi05@gmail.com>
2025-07-10 10:58:20 +0500

    [Change] Изменил имя файла на MainApp

--- Hayat App/MainApp.swift
+++ Hayat App/MainApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainApp: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainApp()
 }
