179e61e73f7fb5db2a4ac88817558cd385669ea2
tolebishka <aitenovtolebi05@gmail.com>
2025-07-10 18:39:30 +0500

    [Create] Create files for profile

---
+++ Hayat App/Model/Profile/ProfileModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileModel.swift
+//  Hayat App
+//
+//  Created by Arcana Inc on 10.07.2025.
+//
+
+import Foundation
---
+++ Hayat App/Views/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Hayat App
+//
+//  Created by Arcana Inc on 10.07.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
