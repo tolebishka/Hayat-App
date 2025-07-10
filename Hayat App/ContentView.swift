a2bca47f531ac8352a97d9ce47a7b57aaa736f9c
tolebishka <aitenovtolebi05@gmail.com>
2025-07-10 10:34:39 +0500

    Initial Commit

---
+++ Hayat App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hayat App
+//
+//  Created by Arcana Inc on 10.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hayat App/Hayat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hayat_AppApp.swift
+//  Hayat App
+//
+//  Created by Arcana Inc on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Hayat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
