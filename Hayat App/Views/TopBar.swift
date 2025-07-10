0e2140cdb0c68d7c314389ac9b74dbf302445bbf
tolebishka <aitenovtolebi05@gmail.com>
2025-07-10 14:31:32 +0500

    [Create] Created TopBar View

---
+++ Hayat App/Views/TopBar.swift
@@ -0,0 +1,20 @@
+//
+//  TopBar.swift
+//  Hayat App
+//
+//  Created by Arcana Inc on 10.07.2025.
+//
+
+import SwiftUI
+
+struct TopBar: View {
+    
+    @State var count: Int = 0
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    TopBar()
+}
